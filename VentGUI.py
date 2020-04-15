bae13d90f0d7c08d9d4eddd149eade776836b217
Michael Madden <michael.madden@nuigalway.ie>
2020-04-15 02:36:38 +0100

    Updated to use signals and slots.
    Updated code to use signals and slots: when data is read from sensor, it emits appropriate signals and slots on GUI display them.

--- VentGUI.py
+++ VentGUI.py
@@ -1,4 +1,10 @@
+# Authors: Michael Madden and Atif Shazad.
+# Developed for the Galway Vent Share project: www.galwayventshare.com
+
+
 from PyQt5 import QtWidgets, QtCore, uic
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 from random import uniform
@@ -12,6 +18,8 @@ REALSENSORS=False     # if True, read data from sensors; if false, generate rand
 interval = 50         # update interval 50ms
 graphPoints = 100     # how many points to display on the graph
 movingWindowSize = 20 # Size of the window for estimation of peak and mean values
+ADDRESS = 0x01        # Address for sensor comms
+
 
 # Simple utility function to round a float to a specified number of digits (defaults to 2) and convert to string
 def floatToStr(value, numDigits=2):
@@ -19,68 +27,66 @@ def floatToStr(value, numDigits=2):
     v = int(value) if (numDigits==0) else round(value, numDigits)
     return str(v)
 
-ADDRESS = 0x01
 
 if REALSENSORS:
-    # The serial port access crashes in Windows - don't access it if sumulating data
+    # The serial port access crashes in Windows - don't access it if simulating data
     ser = serial.Serial(
              port = '/dev/ttyUSB0',          #number of device, numbering starts at zero.
              baudrate=115200,            #baudrate
              bytesize=serial.EIGHTBITS,  #number of databits
              parity=serial.PARITY_NONE,  #enable parity checking
              stopbits=serial.STOPBITS_ONE,  #number of stopbits
-             timeout=1,                  #set a timeout value (example only because reset
-                                #takes longer)
+             timeout=1,                  #set a timeout value (example only because reset takes longer)
              xonxoff=0,                  #disable software flow control
              rtscts=0,                   #disable RTS/CTS flow control
          )
 
 # Define routines for communication with sensor
 
-def get_sw_version(address): # Get software version of cable    
+def get_sw_version(address): # Get software version of cable
     command = bytearray(([address] + [0x01] + [0x00] + [0xB2]))
     ser.write(command)
-    data = ser.read(7)   
+    data = ser.read(7)
     return data.hex()
-    
-def get_hw_version(address): # Default hardware version    
+
+def get_hw_version(address): # Default hardware version
     command = bytearray(([address] + [0x02] + [0x00] + [0x9F]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
-    
-def test_command(address): # Default test command     
+
+def test_command(address): # Default test command
     command = bytearray(([address] + [0x05] + [0x00] + [0x31]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
 
 def get_pressure(address): # Get pressure value from pressure sensor
     command = bytearray(([address] + [0x07] + [0x00] + [0xE8]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     reverse_data = data[::-1]
     pres = reverse_data[1:3]
     Dp = int(pres.hex(),16)
     P = 1.01972*(((Dp-1638)/32.7675)-200) # Apply scaling factor and convert to CM H2O
     return P
-    
+
 def hard_reset_board(address): # Hard reset of comm board on Nicolay cable
     command = bytearray(([address] + [0x0B] + [0x00] + [0x5C]))
     ser.write(command)
-    data = ser.read(4);  
+    data = ser.read(4);
     return data.hex()
 
 def hard_reset_sensor(address): # Hard reset of sensors
     command = bytearray(([address] + [0x0C] + [0x00] + [0xF2]))
     ser.write(command)
-    data = ser.read(4)   
+    data = ser.read(4)
     return data.hex()
-    
+
 def soft_reset_sensor(address): # Soft reset of sensors
     command = bytearray(([address] + [0x0D] + [0x00] + [0x06]))
     ser.write(command)
-    data = ser.read(4)  
+    data = ser.read(4)
     return data.hex()
 
 def start_flowsensor(address): # intialise flow sensor
@@ -104,37 +110,37 @@ def get_flow(address): # Get flow value from flow sensor
     # convert flow to litres per minute
     F = F/1000
     return F
-    
+
 def get_raw_flow(address): # Get raw flow value from flow sensor
     command = bytearray(([address] + [0x11] + [0x00] + [0xDC]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
 
 def get_flowsensor_scale(address): # Get scaling factor from flow sensor
     command = bytearray(([address] + [0x12] + [0x00] + [0xF1]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
-    
+
 def get_flowsensor_offset(address): # Get offset factor from flow sensor
     command = bytearray(([address] + [0x13] + [0x00] + [0x05]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
-    
+
 def get_heater_state(address): # Get current status of heater
     command = bytearray(([address] + [0x14] + [0x00] + [0xAB]))
     ser.write(command)
-    data = ser.read(5)   
+    data = ser.read(5)
     return data.hex()
-    
+
 def get_heater_power(address): # Get current power of heater [in percentage]
     command = bytearray(([address] + [0x15] + [0x00] + [0x5F]))
     ser.write(command)
-    data = ser.read(5)   
+    data = ser.read(5)
     return data.hex()
-    
+
 def set_heater_state(address, state): # Set current status of heater [0: OFF; 1: ON]
     if(state==0): # set heater off
        command = bytearray(([address] + [0x14] + [0x01] + [0x00] + [0xE2]))
@@ -144,9 +150,9 @@ def set_heater_state(address, state): # Set current status of heater [0: OFF; 1:
        command = bytearray(([address] + [0x14] + [0x01] + [0x01] + [0xD3]))
        ser.write(command)
        data = ser.read(5)
-       
+
     return data.hex()
-    
+
 def get_temperature(address): # Get current temperatue [in Celcius]
     command = bytearray(([address] + [0x16] + [0x00] + [0x72]))
     ser.write(command)
@@ -154,55 +160,59 @@ def get_temperature(address): # Get current temperatue [in Celcius]
     reverse_data = data[::-1]
     temp = reverse_data[1:3]
     return temp.hex()
-    
+
 def get_temperature_scale(address): # Get current scaling factor for temperature
     command = bytearray(([address] + [0x18] + [0x00] + [0x1F]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
-    
+
 def get_temperature_offset(address): # Get current offset factor for temperature
     command = bytearray(([address] + [0x19] + [0x00] + [0xEB]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     return data.hex()
 
 def force_temperature_update(address): # Force update of temperature on board calculation
     command = bytearray(([address] + [0x1B] + [0x00] + [0x32]))
     ser.write(command)
-    data = ser.read(6)   
+    data = ser.read(6)
     reverse_data = data[::-1]
     temp = reverse_data[1:3]
     return temp.hex()
-        
+
 
 class MainWindow(QtWidgets.QMainWindow):
+    # Define signals that are emitted when there is new data from the sensors
+    newPress = pyqtSignal(float)
+    newFlow = pyqtSignal(float)
+    newPpeak = pyqtSignal(float)
+    newVte = pyqtSignal(float)
+    newPEEP = pyqtSignal(float)
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        
+
         #Load the UI Page
         uic.loadUi('mainwindow.ui', self)
         self.showFullScreen();
-        
-        # Set up a timer
+
+        # Set up a timer to get new data at fixed intervals
         self.timer = QtCore.QTimer()
         self.timer.setInterval(interval)
-        self.timer.timeout.connect(self.updatePlots)
+        self.timer.timeout.connect(self.updateData)
         self.timer.start()
 
         # Pen that defines the line style for the graphs
         self.linePen = pg.mkPen(color='g', width=3)
 
         # Initialise graphs
-       # self.timeData = list(range(graphPoints)) # numbers 0-99
         self.timeData = array(range(graphPoints))*interval/1000 # time array is values in seconds
-
         self.pressData = [0] * len(self.timeData) # make a list of zeros the same length as the timeData list
         self.flowData = [0] * len(self.timeData) # make a list of zeros the same length as the timeData list
         self.setupPressurePlot(self.timeData, self.pressData)
         self.setupFlowPlot(self.timeData, self.flowData)
-        
+
         # Some additional variables to calculate stats
         # Note - moved some of these from being class variables to instance variables
         self.timeCount = 0
@@ -216,23 +226,27 @@ class MainWindow(QtWidgets.QMainWindow):
         self.negPeaks = [0] * movingWindowSize
         self.PEEP = [0] * 5
         self.expV = [0] * 5
+        self.xSim = 0
 
-        if not REALSENSORS:
-            self.xSim = 0
+        # Connect up signals to slots
+        self.newPress.connect(self.plotPressure)
+        self.newFlow.connect(self.plotFlow)
+        self.newPpeak.connect(self.setPpeak)
+        self.newVte.connect(self.setVte)
+        self.newPEEP.connect(self.setPEEP)
+
 
     def setupPressurePlot(self, hour, press):
         self.pressureLine = self.pressGraphWidget.plot(hour, press, pen=self.linePen)
         self.pressGraphWidget.setEnabled(False) # Disable all interaction - want output-only graph display
+        self.pressGraphWidget.showGrid(x=False, y=True) # Horizontal grid lines including at y=0
 
     def setupFlowPlot(self, hour, flow):
         self.flowLine = self.flowGraphWidget.plot(hour, flow, pen=self.linePen)
         self.flowGraphWidget.setEnabled(False) # Disable all interaction - want output-only graph display
+        self.flowGraphWidget.showGrid(x=False, y=True) # Horizontal grid lines including at y=0
 
-    def updatePlots(self):
-        # Update the timeline - not doing this for now
-        #self.timeData = self.timeData[1:]  # Remove the first y element.
-        #self.timeData.append(self.timeData[-1] + 1)  # Add a new value 1 higher than the last.
-
+    def updateData(self):
         if REALSENSORS:
             # Real mode, not simulation mode: read data from sensors
             flow = get_flow(ADDRESS)
@@ -251,64 +265,66 @@ class MainWindow(QtWidgets.QMainWindow):
                self.PEEP.append(self.prevPress) # PEEP
                self.expV = self.expV[1:]
                self.expV.append(2286*(self.instV/(self.vteTimer*50))) # tidal volume
-               # print(self.vteTimer)
             self.instV = 0
             self.vteTimer = 0
             self.insp = True
             self.Exp = False
-            
-        
+
         if(flow<0 and self.prevFlow>=0): # Detect sero crossing: Postive to negative change
             self.insp = False
             self.Exp = True
-    
-            
+
         if(self.Exp == True): # for estimation of tidal volume
             self.instV += -1*flow
             self.vteTimer += 1
-        
-    
-        self.prevPress = pressure    
+
+        self.prevPress = pressure
         self.prevFlow = flow
 
-        # Update the pressure graph
+        # Emit messages to update pressure and flow graphs
+        self.newPress.emit(pressure)
+        self.newFlow.emit(flow)
+
+        # Record last 20 peak pressure values for moving average
+        self.posPeaks = self.posPeaks[1:]
+        self.posPeaks.append(max(self.pressData))
+
+        # Emit messages to update stats on every 5th call (250ms)
+        self.timeCount += 1
+        if(self.timeCount>5):
+           self.newPpeak.emit(sum(self.posPeaks)/movingWindowSize)
+           self.newVte.emit(sum(self.expV)/5)
+           self.newPEEP.emit(sum(self.PEEP)/5)
+           self.timeCount = 0
+
+    # Update the pressure graph (slot for handling newPress signal)
+    @pyqtSlot(float)
+    def plotPressure(self, pressure):
         self.pressData = self.pressData[1:]  # Remove the first
         self.pressData.append(pressure)  # Add the latest pressure value
         self.pressureLine.setData(self.timeData, self.pressData)  # Update the graph with the new data.
-        self.pressGraphWidget.showGrid(x=False, y=True) # Horizontal grid lines including at y=0
 
-        # Update the flow graph
+    # Update the flow graph (slot for handling newFlow signal)
+    @pyqtSlot(float)
+    def plotFlow(self, flow):
         self.flowData = self.flowData[1:]  # Remove the first
         self.flowData.append(flow)  # Add the latest flow value
         self.flowLine.setData(self.timeData, self.flowData)  # Update the graph with the new data.
-        self.flowGraphWidget.showGrid(x=False, y=True) # Horizontal grid lines including at y=0
-        
-        # Record last 20 peak pressure values and average
-        self.posPeaks = self.posPeaks[1:]
-        self.posPeaks.append(max(self.pressData))
-        
-        self.timeCount += 1
 
-        # Update stats
-        if(self.timeCount>5): # update every 5th call (250ms)
-           self.setPpeak(sum(self.posPeaks)/movingWindowSize)
-           self.setVte(sum(self.expV)/5)
-           self.setPEEP(sum(self.PEEP)/5)
-           self.timeCount = 0
-    
-    # Change Ppeak value
+    # Change Ppeak value (slot for handling newPpeak signal)
+    @pyqtSlot(float)
     def setPpeak(self, value):
         self.valPpeak.setText(floatToStr(value,1))
 
-    # Change Vte value
+    # Change Vte value (slot for handling newVte signal)
+    @pyqtSlot(float)
     def setVte(self, value):
         self.valVte.setText(floatToStr(value,1))
 
-    # Change PEEP value
+    # Change PEEP value (slot for handling newPEEP signal)
+    @pyqtSlot(float)
     def setPEEP(self, value):
         self.valPeep.setText(floatToStr(value,1))
-
-
 
     # Quit out of the app by pressing ESC key
     def keyPressEvent(self, e):
@@ -327,11 +343,13 @@ def main():
     # Launch the application window
     app = QtWidgets.QApplication(sys.argv)
     QtWidgets.QApplication.setOverrideCursor(QtCore.Qt.BlankCursor) # stop the cursor being displayed
-
     window = MainWindow()
     window.show()
+
+    # Run until the exit message
     sys.exit(app.exec_())
-    ser.close()
+    if REALSENSORS:
+        ser.close()
 
 if __name__ == '__main__':
     main()
